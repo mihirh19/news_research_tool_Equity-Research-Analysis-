e8ee297786c81242bd1b39a9ac5fa0046ad5a5ef
mihirh19 <miheerhadvani990@gmail.com>
2024-05-17 09:10:57 +0530

    Refactor URL input and add error handling for loading data and processing queries

--- main.py
+++ main.py
@@ -20,10 +20,7 @@ st.sidebar.title("News Article URLs")
 urls = []
 number_of_urls = st.sidebar.number_input(label="Number of Products",
                                                 min_value=0, max_value=20, value=1)
-
-for i in range(number_of_urls):
-   url = st.sidebar.text_input(f"URL {i+1}")
-   urls.append(url)
+urls = [st.sidebar.text_input(f"URL {i+1}") for i in range(number_of_urls)]
 
 
 process_url_clicked = st.sidebar.button("Process URLs")
@@ -33,47 +30,61 @@ main_placeholder = st.empty()
 # llm = HuggingFaceEndpoint(repo_id = "meta-llama/Meta-Llama-3-8B", 
 #                           max_length=500, token=st.secrets["HUGGINGFACEHUB_API_TOKEN"])
 # llm = Ollama(model="llama3")
-llm = ChatCohere(model="command", max_tokens=256, temperature=0.75, cohere_api_key=st.secrets["COHERE_API_KEY"])
+llm = ChatCohere(model="command-r-plus", max_tokens=256, temperature=0.75, cohere_api_key=st.secrets["COHERE_API_KEY"])
 embeddings = CohereEmbeddings(cohere_api_key=st.secrets["COHERE_API_KEY"])
 
 if process_url_clicked:
-   
+   if all(urls):
+      try:
    # load data
-   loader = WebBaseLoader(
-      web_paths=urls
-      
-   )
-   main_placeholder.text("Data Loading...Started...✅✅✅")
-   data = loader.load()
-   # split data
-   text_splitter = RecursiveCharacterTextSplitter(
-      separators=['\n\n', '\n', '.', ','],
-      chunk_size=1000
-   )
-   main_placeholder.text("Text Splitter...Started...✅✅✅")
-   docs = text_splitter.split_documents(data)
-   # create embeddings and save it to FAISS index
+         loader = WebBaseLoader(
+            web_paths=urls
+            
+         )
+         with st.status("Loading Data...", expanded=True) as status:
+            st.text("Data Loading...Started...✅✅✅")
+            data = loader.load()
+            # split data
+            text_splitter = RecursiveCharacterTextSplitter(
+               separators=['\n\n', '\n', '.', ','],
+               chunk_size=1000
+            )
+            st.text("Text Splitter...Started...✅✅✅")
+            docs = text_splitter.split_documents(data)
+            # create embeddings and save it to FAISS index
 
-   vectorstore_openai = FAISS.from_documents(docs, embeddings)
-   main_placeholder.text("Embedding Vector Started Building...✅✅✅")
-   time.sleep(2)
-   vectorstore_openai.save_local(file_path)
+            vectorstore_openai = FAISS.from_documents(docs, embeddings)
+            st.text("Embedding Vector Started Building...✅✅✅")
+            time.sleep(2)
+            vectorstore_openai.save_local(file_path)
+            status.update(label="Processing complete! ✅ ", state='complete', expanded=False)
+         time.sleep(2)
+         
+      except Exception as e:
+            main_placeholder.error(f"An error occurred: {e}")
+   else:
+      main_placeholder.error("Please enter all URLs.")
    # vectorstore = vectorstore_openai.as_retriever() 
    
    
-query = main_placeholder.text_input("Question: ")
+query = st.text_input("Question: ")
 submit = st.button("Submit")
 if submit:
    if query:
-      vectorIndex = FAISS.load_local(file_path, embeddings, allow_dangerous_deserialization=True)
-      chain = RetrievalQAWithSourcesChain.from_llm(llm=llm, retriever=vectorIndex.as_retriever())
-      result = chain.invoke({"question": query}, return_only_outputs=True)
-               # result will be a dictionary of this format --> {"answer": "", "sources": [] }
-      st.header("Answer")
-      st.write(result["answer"])
-      sources = result.get("sources", "")
-      if sources:
-         st.subheader("Sources:")
-         sources_list = sources.split("\n")  # Split the sources by newline
-         for source in sources_list:
-            st.write(source)
+      try:
+         vectorIndex = FAISS.load_local(file_path, embeddings, allow_dangerous_deserialization=True)
+         chain = RetrievalQAWithSourcesChain.from_llm(llm=llm, retriever=vectorIndex.as_retriever())
+         result = chain.invoke({"question": query}, return_only_outputs=True)
+                  # result will be a dictionary of this format --> {"answer": "", "sources": [] }
+         st.header("Answer")
+         st.write(result["answer"])
+         sources = result.get("sources", "")
+         if sources:
+            st.subheader("Sources:")
+            sources_list = sources.split("\n")  # Split the sources by newline
+            for source in sources_list:
+               st.write(source)
+      except Exception as e:
+            st.error(f"An error occurred: {e}")
+   else:
+      st.error("Please enter a query.")
